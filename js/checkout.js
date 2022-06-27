d9a7a85ecee9b553125b7258b902579dc83ca7b1
jonasattiah <info@jonasattiah.de>
2022-06-27 02:13:22 +0200

    19-add-remove-floating-basket-cookie-after-order-checked-out

--- js/checkout.js
+++ js/checkout.js
@@ -111,6 +111,7 @@ let stripe = {
 const checkoutView = {
   name: "checkout",
   init: () => {
+    removeCookie("showFloatingBasket")
     hideFloatingBasket(true);
     const elErrors = document.getElementById("checkout-errors");
     const elPaymentErrors = document.getElementById("payment-errors");
